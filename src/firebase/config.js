39cd39ecee9ab39bbc41fb35e4a93fec3566d83d
Michał Urbanek <m.urbanek@devesoft.pl>
2018-11-23 17:44:16 +0100

    inisital commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBsVVpEDrlNPEmshLcmOuE0FxhjPn0AqMg',
-  authDomain: 'todo-react-redux.firebaseapp.com',
-  databaseURL: 'https://todo-react-redux.firebaseio.com',
-  storageBucket: 'firebase-todo-react-redux.appspot.com'
-};
+   apiKey: "AIzaSyDJV6Hr2SOk2KNDYXHbKxPJZ23eD7E9yYM", 
+   authDomain: "life-organizer-6be5a.firebaseapp.com", 
+   databaseURL: "https://life-organizer-6be5a.firebaseio.com", 
+   projectId: "life-organizer-6be5a", storageBucket: "life-organizer-6be5a.appspot.com", 
+   messagingSenderId: "798549340589" };
